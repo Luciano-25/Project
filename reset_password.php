0f8435aef31d01b69bc93d1b137ac977b5c2a5c3
Abarna Muthu <1231203277@mmu.edu.my>
2025-06-20 15:33:08 +0800

    reset passwd

---
+++ reset_password.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <form method="POST">
+        <h2>Reset Password</h2>
+        <label>New Password:</label>
+        <input type="password" name="password" required>
+        <button type="submit">Reset Password</button>
+    </form>
+</body>
+</html>
